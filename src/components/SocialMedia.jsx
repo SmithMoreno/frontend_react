0174cac312bcac27e1c5204e8254a2761be264d2
SmithMoreno <devsagamobile@gmail.com>
2024-12-03 22:59:45 -0500

    url  social rtedes update

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import { BsTwitter,  BsLinkedin, BsGithub, } from "react-icons/bs";
 
-
+ const url= {
+   twitter: "https://x.com/maick_moren",
+   linkedin: "https://www.linkedin.com/in/ewirlin-smith-83773a280/",
+   github: "https://github.com/DevSaga532/DevSaga532"
+ }
 const SocialMedia = () => (
+  
   <div className="app__social">
     
     <div>
-      <BsTwitter />
+      <BsTwitter onClick={() => window.open(url.twitter, "_blank")}   />
     </div>
     <div>
-      <BsLinkedin />
+      <BsLinkedin onClick={() => window.open(url.linkedin, "_blank")} />
     </div>
     <div>
-      <BsGithub />
+      <BsGithub onClick={() => window.open(url.github, "_blank")} />
     </div>
 
    
